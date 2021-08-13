1d6c540dee837b797b10a7d9a8b590135a51a8c5
Binnie179 <88896504+Binnie179@users.noreply.github.com>
2021-08-13 23:34:40 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "He luu bé iu!",
+  text2: "Anh có điều này muốn nói với bé.",
+  text3: "Anh xin lỗi:<",
+  text4: "Bé đừng giận anh nữa nhé <33",
+  text5: "Khôngg",
+  text6: "Tạm tha đó <3",
+  text7: "Bé yêu anh lắm phải không??",
+  text8: "Nói yêu anh đi<3",
+  text9: "Em yêu Mè nhiều lắm",
+  text10: "Anh biết mà ^^ Yêu bé",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Hết dịch anh ra thăm bé nhé <3",
   text12: "Okii lunn <3",
 };
 
